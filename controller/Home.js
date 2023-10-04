53ba776885b6c107c268ccebc172749bd5695770
Dagim Asnake <dagimasnake1@gmail.com>
2023-10-04 00:13:35 -1200

    finished home

--- controller/Home.js
+++ controller/Home.js
@@ -21,3 +21,55 @@ module.exports.CreateHome = async function (req, res) {
           msg: "Home sent successfully"
       }).status(200)
   }
+
+  module.exports.getHome = async function (req, res) {
+    try {
+      const home = await Home.findOne({});
+  
+      if (!home) {
+        return res.status(404).json({
+          error: 'No home found',
+        });
+      }
+  
+      const data = {
+        _id: home._id,
+        title: home.title,
+        quote: home.quote,
+        image: home.image,
+      };
+  
+      return res.status(200).json(data);
+    } catch (error) {
+      console.error('Error retrieving home:', error);
+      return res.status(500).json({
+        error: 'Server error',
+      });
+    }
+  };
+
+
+  module.exports.updateHome = async function (req, res) {
+    const { homeId } = req.params;
+    const data = req.body;
+
+    const updatedData = {
+        title: data.title,
+        quote: data.quote,
+        image: data.image
+    }
+  
+    try {
+      const home = await Home.findById(homeId);
+  
+      if (!home) {
+        return res.status(404).json({ msg: 'Home not found' });
+      }
+  
+      await Home.findByIdAndUpdate(homeId, updatedData);
+      return res.status(200).json({ msg: 'Home updated successfully' });
+    } catch (error) {
+      console.error('Error updating home:', error);
+      return res.status(500).json({ error: 'Server error' });
+    }
+  };
--- route/Home.js
+++ route/Home.js
@@ -2,5 +2,7 @@ const router = require("express").Router();
 const HomeController = require("../controller/Home")
 
 router.post("/post", HomeController.CreateHome)
+router.get('/get', HomeController.getHome)
+router.put('/update/:homeId', HomeController.updateHome)
 
 module.exports = router;
